02ae6b37907ef36ecc62864f5f5184ff2f2b073e
Bonnak <chea.bonnak@gmail.com>
2019-03-23 10:39:41 +0700

    Remove as a functional component

--- src/components/Badge/index.js
+++ src/components/Badge/index.js
@@ -1,30 +1,15 @@
-const colorClass = color => {
-  if (color.toLowerCase() === 'blue') {
-    return 'bg-blue text-white'
-  } else if (color.toLowerCase() === 'red') {
-    return 'bg-red text-white'
-  } else if (color.toLowerCase() === 'green') {
-    return 'bg-green text-white'
-  }
-}
-
 export default {
   name: 'BaseBadge',
-  functional: true,
   props: {
     color: {
       type: String,
       default: ''
     }
   },
-  render(h, context) {
+  render(h) {
     return (
-      <span
-        class="p-1 mt-1 rounded inline-block font-bold leading-none text-center align-baseline whitespace-no-wrap text-xs"
-        {...{ class: context.data.staticClass }}
-        {...{ class: colorClass(context.props.color) }}
-      >
-        {context.children}
+      <span class="p-1 mt-1 rounded inline-block font-bold leading-none text-center align-baseline whitespace-no-wrap text-xs">
+        {this.$slots.default}
       </span>
     )
   }
