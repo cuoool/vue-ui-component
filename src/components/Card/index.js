d433b2a76c83c15b33f384aa5d183c34dc1f1395
Bonnak <chea.bonnak@gmail.com>
2019-03-24 09:10:49 +0700

    Fix card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,5 @@
 export default {
   name: 'BaseCard',
-  props: {
-    color: {
-      type: String,
-      default: ''
-    }
-  },
   render(h) {
     return (
       <div class="rounded overflow-hidden shadow-lg bg-white p-4 w-full">
